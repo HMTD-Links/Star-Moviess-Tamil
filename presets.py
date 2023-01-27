d9a2451b6852739d175f44ec62840f78fbf8b16d
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-27 06:21:31 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,4 +1,4 @@
 class Presets(object):
-    USERS_LIST = "<b>Total:</b>\n\n<b>Subscribers - {}\nBlocked / Deleted - {}</b>"
+    USERS_LIST = "<b>📊 Stats :-</b>\n\n<b>Total Users :- {}\nBlocked / Deleted :- {}</b>"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<b>Use This Command as a Reply to any Telegram Message Without any Spaces.</b>"
