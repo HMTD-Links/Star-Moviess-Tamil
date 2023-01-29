105f54af12f6c59aadd565263be6d96ddc8b1dc7
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-29 14:14:57 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,4 +1,43 @@
 class Presets(object):
+
+      # Start Text
+    START = """<b>Hi 👋🏻 {mention},
+
+I'm UK Studios Official a Bot to Maintain Your Channels. I am very useful for the Channel Admin who have many Channels.
+
+See /help for more Details.
+
+Maintained By : [Karthik](https://t.me/HMTD_Karthik)</b>"""             
+
+      # Help Text
+    HELP = """<b>The Ultimate Tool to Automate Custom Telegram Message Forwarding. Live Syncer, Auto Poster, Backup Bot, Cloner, Chat Forwarder, Duplicator, ... Call it whatever you like! tgcf can fulfill Your Custom Needs.</b>"""
+
+      #About Text
+    ABOUT = """
+<b>🤖 My Name : [UK Auto Forward Bot](https://t.me/UK_Auto_Forward_Bot)
+
+🧑🏻‍💻 Developer : [Karthik](https://t.me/HMTD_Karthik)
+
+📝 Language : Pyrogram
+
+📚 Framework : Python3
+
+📡 Hosted on : VPS
+
+📢 Updates Channel : [UK Movies Zone (Updates)](https://t.me/UK_Movies_Zone_Updates)
+
+🌐 Website : [www.HMTDMovies.tk](https://www.HMTDMovies.tk)</b>"""
+
+      #Support Text
+    SUPPORT = """Hi {mention},"""
+
+      # Alien Covenant Movie
+    ALIEN_COVENANT = """<b> Hi {mention},
+
+Alien Covenant (2017) (BluRay + Original Audios) - [1080p & 720p - x264 - [Tamil + Telugu + Hindi + English] - (DD5.1 - 448Kbps) - 3GB & 1.5GB - 1.4GB | x264 - [Tamil + Telugu + Hindi] - 450MB] - ESub]
+
+📢 Updates Channel : [Star Movies Tamil](https://t.me/Star_Moviess_Tamil)</b>"""
+
     USERS_LIST = "<b>📊 Stats :-</b>\n\n<b>Total Users :- {}\nBlocked / Deleted :- {}</b>"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<b>Use This Command as a Reply to any Telegram Message Without any Spaces.</b>"
